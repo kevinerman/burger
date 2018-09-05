b64bd61f6271f5737d43d4157e8d3129bcd01d0d
Kevin Erman <kevinerman@gmail.com>
2018-09-04 19:35:28 -0500

    Database created

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', False);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Burger', False);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rodeo Burger', True);
